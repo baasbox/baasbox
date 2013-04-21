bdaf90b4ed26e8c5c7c576c58170253ccc0da416
giastfader <claudio.github@gmail.com>
2013-04-21 15:22:14 +0200

    added the api to give to the admin the possibility to set configuration values. It is /admin/configuration/:section/:subSection/:key/:value

---
+++ app/com/baasbox/exception/ConfigurationException.java
@@ -0,0 +1,24 @@
+package com.baasbox.exception;
+
+public class ConfigurationException extends Exception {
+
+	public ConfigurationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
