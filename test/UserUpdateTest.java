0b485e1a9c4b5d0898fcc7f7020346b4f6d70c4f
unknown <claudio.github@gmail.com>
2013-11-11 18:55:19 +0100

    Added test for role update

--- test/UserUpdateTest.java
+++ test/UserUpdateTest.java
@@ -17,16 +17,23 @@
 
 // @author: Marco Tibuzzi
 
+import static org.junit.Assert.fail;
 import static play.test.Helpers.HTMLUNIT;
+import static play.test.Helpers.POST;
 import static play.test.Helpers.PUT;
 import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.route;
 import static play.test.Helpers.routeAndCall;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
+import java.util.UUID;
+
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Test;
 
 import play.libs.F.Callback;
@@ -132,6 +139,49 @@ public class UserUpdateTest extends AbstractUserTest
 	        }
 		);
 	}
+	
+	@Test
+	public void testUserChangeRole(){
+		running
+		(
+			fakeApplication(), 	new Runnable() 	{
+				public void run() 	{
+					try {
+						//create a user
+						String userName = "fake"+UUID.randomUUID().toString();
+						String sFakeCreateUser = "/admin/user/"+ userName;
+						FakeRequest requestCreation = new FakeRequest(POST, sFakeCreateUser);
+	                    requestCreation = requestCreation.withHeader(TestConfig.KEY_APPCODE, TestConfig.VALUE_APPCODE);
+	                    requestCreation = requestCreation.withHeader(TestConfig.KEY_AUTH, TestConfig.AUTH_ADMIN_ENC);
+	                    ObjectMapper mapper = new ObjectMapper();
+	                    JsonNode actualObj = mapper.readTree("{\"username\":\""+userName+"\","
+	                    		+ "\"password\":\"test\","	
+	                    		+ "\"role\":\"registered\"}");
+	                    requestCreation = requestCreation.withJsonBody(actualObj);
+	                    requestCreation = requestCreation.withHeader("Content-Type", "application/json");
+	                    Result result = route(requestCreation);
+	                    assertRoute(result, "testRoleCreate.createUser", Status.CREATED, null, true);
+	                   
+						//change its role
+	                    requestCreation = new FakeRequest(PUT, sFakeCreateUser);
+	                    requestCreation = requestCreation.withHeader(TestConfig.KEY_APPCODE, TestConfig.VALUE_APPCODE);
+	                    requestCreation = requestCreation.withHeader(TestConfig.KEY_AUTH, TestConfig.AUTH_ADMIN_ENC);
+	                    mapper = new ObjectMapper();
+	                    actualObj = mapper.readTree("{\"role\":\"administrator\"}");
+	                    requestCreation = requestCreation.withJsonBody(actualObj);
+	                    requestCreation = requestCreation.withHeader("Content-Type", "application/json");
+	                    result = route(requestCreation);
+	                    assertRoute(result, "testRoleCreate.createUser", Status.OK, "\"role\":\"administrator\"", true);
+						//check the role is changed
+						//drop the user
+                    }catch (Exception e) {
+                		e.printStackTrace();
+                		fail(e.getMessage());
+				}						
+				}
+			});
+	}
+
 	
 	//@After
 	public void afterTest()
