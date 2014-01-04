9f1cc1f51bbb06d4ed2456da106f9d2ea097256e
unknown <claudio.github@gmail.com>
2014-01-04 19:18:15 +0100

    store the edges' id

--- app/com/baasbox/controllers/Link.java
+++ app/com/baasbox/controllers/Link.java
@@ -8,11 +8,23 @@ import com.baasbox.controllers.actions.filters.ConnectToDBFilter;
 import com.baasbox.controllers.actions.filters.UserCredentialWrapFilter;
 import com.baasbox.dao.exception.DocumentNotFoundException;
 import com.baasbox.service.storage.LinkService;
+import com.orientechnologies.orient.core.record.impl.ODocument;
 
 public class Link extends Controller{
 
 	@With ({UserCredentialWrapFilter.class,ConnectToDBFilter.class})
 	public static Result createLink(String sourceId, String destId, String edgeName){
+		ODocument link = null;
+		try {
+			link = LinkService.createLink(sourceId, destId, edgeName);
+		}catch (DocumentNotFoundException e){
+			return notFound("Source or Destination record not found. Hint: do you have the read grant on them?");
+		}
+		return ok(link.toJSON());
+	}
+	
+	@With ({UserCredentialWrapFilter.class,ConnectToDBFilter.class})
+	public static Result getLink(String sourceId, String destId, String edgeName){
 		try {
 			LinkService.createLink(sourceId, destId, edgeName);
 		}catch (DocumentNotFoundException e){
--- app/com/baasbox/service/storage/LinkService.java
+++ app/com/baasbox/service/storage/LinkService.java
@@ -1,18 +1,34 @@
 package com.baasbox.service.storage;
 
+import java.util.UUID;
+
 import com.baasbox.dao.exception.DocumentNotFoundException;
+import com.orientechnologies.orient.core.record.impl.ODocument;
+import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientEdge;
 import com.tinkerpop.blueprints.impls.orient.OrientVertex;
 
 public class LinkService {
 
-	public static void createLink(String sourceId, String destId,String edgeName) throws DocumentNotFoundException {
+	public static ODocument createLink(String sourceId, String destId,String edgeName) throws DocumentNotFoundException {
+		OrientVertex sourceVertex = StorageUtils.getNodeVertex(sourceId);
+		OrientVertex destVertex = StorageUtils.getNodeVertex(destId);
+		UUID token = UUID.randomUUID();
+		OrientEdge edge = (OrientEdge)sourceVertex.addEdge(edgeName, destVertex);
+		edge.getRecord().field(BaasBoxPrivateFields.ID.toString(),token.toString());
+		edge.save();
+		//TODO: when we will support transactions, this will have to be changed 
+		edge.getGraph().commit();
+		return edge.getRecord();
+	}
+	
+	public static void getLink(String sourceId, String destId,String edgeName) throws DocumentNotFoundException {
 		OrientVertex sourceVertex = StorageUtils.getNodeVertex(sourceId);
 		OrientVertex destVertex = StorageUtils.getNodeVertex(destId);
 		
 		OrientEdge edge = (OrientEdge)sourceVertex.addEdge(edgeName, destVertex);
 		edge.save();
-		//TODO: when we will support transactions, this must be changed 
+		//TODO: when we will support transactions, this will have to be changed 
 		edge.getGraph().commit();
 	}
 
