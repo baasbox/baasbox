ef65e6a4f3b3caf11fa8bb27f4834ac5f7402f2d
Claudio <claudio.github@gmail.com>
2014-05-09 15:30:05 +0200

    new baas sbt task to have a zip package similar to play 2.1.x

---
+++ distfiles/start.bat
@@ -0,0 +1,1 @@
+java %* -cp "./lib/*;" paly.core.server.NettyServer .
--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,9 @@ import Keys._
 import play.Project._
 import com.typesafe.config._
 import com.typesafe.sbteclipse.core._
+import com.typesafe.sbt.packager.Keys.stage
+import com.typesafe.sbt.SbtNativePackager.Universal
+import java.io.File
 
 object ApplicationBuild extends Build {
 	override def settings = super.settings ++ Seq(
@@ -10,11 +13,16 @@ object ApplicationBuild extends Build {
     )
   
 	val conf = ConfigFactory.parseFile(new File("conf/application.conf")).resolve()
-    
 	val appName         = "BaasBox"
-    val appVersion      = conf.getString("api.version")
+  val appVersion      = conf.getString("api.version")
 
-    val appDependencies = Seq(
+  // fixme move version configuration here to make it dinamic
+  //val altConf =ConfigFactory.parseFile(new File("conf/application.conf")).withValue("api.version",ConfigValueFactory.fromAnyRef("0.8.0-SNAPSHOT-2"))
+
+  //todo script->distfiles
+  //todo move package to -> dist
+
+  val appDependencies = Seq(
     			javaCore,filters,cache,
     			"commons-io" % "commons-io" % "2.4",
     			"commons-lang" % "commons-lang" % "2.6",
@@ -23,27 +31,56 @@ object ApplicationBuild extends Build {
     			"org.imgscalr" % "imgscalr-lib" % "4.2",
     			"org.apache.commons" % "commons-email" % "1.3.1",
     			"org.antlr" % "stringtemplate" % "4.0.2",
-                "com.github.tony19" % "named-regexp" % "0.2.3",
-                "org.scribe" % "scribe" % "1.3.2",
-				"com.eaio.uuid" % "uuid" % "3.4",
-				"org.apache.tika" % "tika-core" % "1.4",
-				"org.apache.tika" % "tika-parsers" % "1.4",
-                  "com.codahale.metrics" % "metrics-json" % "3.0.1",
-    "com.codahale.metrics" % "metrics-annotation" % "3.0.1"
+          "com.github.tony19" % "named-regexp" % "0.2.3",
+          "org.scribe" % "scribe" % "1.3.2",
+				  "com.eaio.uuid" % "uuid" % "3.4",
+				  "org.apache.tika" % "tika-core" % "1.4",
+				  "org.apache.tika" % "tika-parsers" % "1.4",
+          "com.codahale.metrics" % "metrics-json" % "3.0.1",
+          "com.codahale.metrics" % "metrics-annotation" % "3.0.1"
     		//	,"com.wordnik" %% "swagger-play2" % "1.2.1-SNAPSHOT",
     		//	"com.wordnik" %% "swagger-play2-utils" % "1.2.1-SNAPSHOT",
-
     )
+
+    val baas = taskKey[File]("distribute standard baasbox format")
 
     val main = play.Project(appName, appVersion, appDependencies).settings(
        sources in doc in Compile := List(),
-	resolvers := Seq(
+	     resolvers := Seq(
           "sonatype-releases" at "https://oss.sonatype.org/content/repositories/releases",
           "sonatype-snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
-		  "eaio" at "http://eaio.com/maven2"
-	    )
-	  ,requireJs += "main.js"
-	  ,Keys.fork in (Test) := false
+		      "eaio" at "http://eaio.com/maven2"
+	      )
+
+       ,baas := {
+          val distributionName = name.value.toLowerCase + "-" + version.value
+		  val baseTarget = file(target.value.getAbsolutePath) / "universal"
+          val tmpDir = baseTarget / distributionName
+          val distributionDir = file(baseDirectory.value.getAbsolutePath) / "dist"
+          val zipFile = distributionDir / (distributionName + ".zip")
+          IO.delete(tmpDir)
+          IO.delete(zipFile)
+          val scriptsDir = file(baseDirectory.value.getAbsolutePath) / "distfiles"
+          val staging = baseTarget / "stage"
+          stage.value
+
+          IO.copyDirectory(staging,tmpDir)
+          IO.copyDirectory(scriptsDir,tmpDir)
+          IO.delete(tmpDir/"bin")
+
+          def entries(f : File):List[File] =
+            f :: (if (f.isDirectory) IO.listFiles(f).toList.flatMap(entries(_)) else Nil)
+          IO.zip(entries(tmpDir).map(
+            d => (d,d.getAbsolutePath.substring(tmpDir.getParent.length+1))),
+            zipFile)
+          IO.delete(tmpDir)
+          println("done packaging baas server distribution!!!")
+          zipFile
+        }
+       , resourceDirectory in Compile <<= baseDirectory(_ => new File("conf"))
+       , mappings in Universal ~= { _.filterNot { case (_,name) => name.startsWith("conf")}}
+	     ,requireJs += "main.js"
+	     ,Keys.fork in (Test) := false
     )
 
 }
