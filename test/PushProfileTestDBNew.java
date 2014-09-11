552b82fe1a6d0acf782f2c44cd2bb5ab061372b5
Claudio <claudio.github@gmail.com>
2014-09-11 13:05:17 +0200

    test was not compatible with JVM 1.6

--- test/PushProfileTestDBNew.java
+++ test/PushProfileTestDBNew.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.assertTrue;
 import static play.test.Helpers.HTMLUNIT;
 import static play.test.Helpers.DELETE;
+import static play.test.Helpers.routeAndCall;
 import static play.test.Helpers.running;
 
 import java.util.HashMap;
@@ -8,6 +9,9 @@ import java.util.HashMap;
 import org.junit.Before;
 
 import play.libs.F.Callback;
+import play.mvc.Result;
+import play.mvc.Http.Status;
+import play.test.FakeRequest;
 import play.test.TestBrowser;
 import core.TestConfig;
 
@@ -19,20 +23,21 @@ public class PushProfileTestDBNew extends PushProfileAbstractTest {
 	@Before
 	public void beforeTest(){
 		//import db
-		running	(
-			getTestServer(), 
-			HTMLUNIT, 
-			new Callback<TestBrowser>()  {
-				public void invoke(TestBrowser browser) {
-					//load a backup we did using v.0.8.3
-					setHeader(TestConfig.KEY_APPCODE, TestConfig.VALUE_APPCODE);
-					setHeader(TestConfig.KEY_AUTH, TestConfig.AUTH_ADMIN_ENC);
-					setMultipartFormData();
-					setAssetFile("/BB_export_083_push_test_empty.zip", "application/zip");
-					int status = httpRequest("http://localhost:3333/admin/db/1000", DELETE,new HashMap<String,String>());
-					assertTrue(status==200);	
-				}//invoke
-			}//Callback<TestBrowser>() 
+		running
+		(
+			getFakeApplication(), 
+			new Runnable() 
+			{
+				public void run() 
+				{
+					String sAuthEnc = TestConfig.AUTH_ADMIN_ENC;
+					FakeRequest request = new FakeRequest("DELETE", "/admin/db/100");
+					request = request.withHeader(TestConfig.KEY_APPCODE, TestConfig.VALUE_APPCODE);
+					request = request.withHeader(TestConfig.KEY_AUTH, sAuthEnc);
+					Result result = routeAndCall(request);
+					assertRoute(result, "testDelete", Status.OK, null, true);
+				}//run
+			}//Runnable() 
 		);//running
 	}//beforeTest()
 
