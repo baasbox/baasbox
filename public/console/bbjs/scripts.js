9a7b22e6578f7531d5d02dfd304cbc068a157ee1
Claudio <claudio.github@gmail.com>
2014-10-07 15:24:53 +0200

    show code for selected plugin

--- public/console/bbjs/scripts.js
+++ public/console/bbjs/scripts.js
@@ -10,17 +10,67 @@ function loadScriptsPage(scopeName){
 
 
 function ScriptsController($scope){
-	var _this = $scope;
+	$scope.selected=0;
+	$scope.data={};
+	
+	$scope.selectItem = function(s){
+		$scope.selected=s;
+	}
+	
+	$scope.getSelectedItem = function(){
+		return $scope.selected;
+	}
+	
+	$scope.reload=function(){
+		BBRoutes.com.baasbox.controllers.ScriptsAdmin.list().ajax({
+			success: function(data) {
+				$scope.$apply(function(){
+					$scope.data=data;
+				});
+			}
+		});
+	}
+	
+	$scope.remove=function(index){
+		var name=$scope.data.data[index].name;
+		if (confirm("This will permanently delete the plugin from the server. Are you sure?")){
+			BBRoutes.com.baasbox.controllers.ScriptsAdmin.drop(name).ajax({
+				success: function(data) {
+					$scope.reload();
+				}
+			});
+		}
+	}//$scope.remove()
+	
 }
 
+/*
 angular.module('console', [])
 	.directive('jsCodeHighlight', function($rootScope){
 	    return {
 	        restrict: 'A',
 	        scope:false,
 	        link: function(scope,elm,attrs){
-	        	elm.html(scope.script.code[0]);
+	        	console.log("direttiva!");
+	        	//elm.html(scope.script.code[0]);
 	            elm.snippet("javascript",{style:"whitengrey"});
 	        }
 	    }
 	});
+*/
+
+angular.module('console', [])
+	.directive('snippet', ['$timeout', '$interpolate', function($timeout, $interpolate) {
+		return {
+	        restrict: 'E',
+	        template:'<pre><code ng-transclude></code></pre>',
+	        replace:true,
+	        transclude:true,
+	        link:function(scope, elm, attrs){             
+	            var tmp =  $interpolate(elm.find('code').text())(scope);
+	             $timeout(function() {                
+	                elm.find('code').html(hljs.highlightAuto(tmp).value);
+	              }, 0);
+	        }
+	    };
+	}]);
