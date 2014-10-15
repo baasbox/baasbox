8f8c943c6d23d60c9e41f958dfe25a2891d19e8a
Claudio <claudio.github@gmail.com>
2014-10-15 13:05:51 +0200

    Better management of some users' attributes
    The id of social networks now are in the visiblyByRegisteredUsers._social data structure
    The data collected from the social networks are stored in the visibleByTheUser field

--- test/SocialTest.java
+++ test/SocialTest.java
@@ -7,6 +7,14 @@ public class SocialTest {
 		//TEST the the after a Signup using a social network, the user data are returned 
 		//issue #504
 		
+		//there is the signupdate in system object?
+		
+		//users cannot access to system properties belonging to other users
+		
+		//users have social data property	
+		
+		//users cannot update their _social fields
+		
 	}
 
 }
