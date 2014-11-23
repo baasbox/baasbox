2efffbf7a999a8e07a4597e5a9c7e14b8931f26c
Claudio <claudio.github@gmail.com>
2014-11-23 01:32:03 +0100

    revert console document list screen

--- public/console/bbjs/documents-datatable.js
+++ public/console/bbjs/documents-datatable.js
@@ -37,7 +37,7 @@ var documentDataArray= new Array();
 			               },
 			               {"mData": "id","mRender": function ( data, type, full ) {
 							   var obj=JSON.parse(JSON.stringify(full));
-			            	   return getActionButton("edit","document",data + obj["@class"]) + "&nbsp;" + getActionButton("delete","document",data+obj["@class"]) + "&nbsp;" + getActionButton("acl","document",data+obj["@class"]);
+			            	   return getActionButton("edit","document",data + obj["@class"]) + "&nbsp;" + getActionButton("delete","document",data+obj["@class"]) /*+ "&nbsp;" + getActionButton("acl","document",data+obj["@class"])*/;
 			            	},bSortable:false,bSearchable:false
 			               }
 			               ],
