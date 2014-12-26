7dd21a0a138e976fef00a0bce4105a5d842a301b
Claudio <claudio.github@gmail.com>
2014-12-26 17:13:21 +0100

    cache the tag permissions

--- app/com/baasbox/BBCache.java
+++ app/com/baasbox/BBCache.java
@@ -7,7 +7,10 @@ import play.cache.Cache;
 public class BBCache {
 
 	private static String UUID_KEY="uuid.";
-	public static int UUID_DEFAULT_TIMEOUT=3600; //seconds
+	public static int UUID_TIMEOUT=0; //seconds, 0=unlimited
+	public static String TAG_KEY="tag_";
+	public static int TAG_TIMEOUT=120;
+	
 
 	public static void cacheUUIDtoRID(String uuid, String rid){
 		if (!rid.startsWith("#-")) //in case of transaction, just created RIDs are like #-2:1
