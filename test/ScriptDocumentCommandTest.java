474c138a45a85e136897b39944073851358abc5b
Claudio <claudio.github@gmail.com>
2015-03-09 16:37:00 +0100

    fixed #682

--- test/ScriptDocumentCommandTest.java
+++ test/ScriptDocumentCommandTest.java
@@ -2,29 +2,40 @@
  * Created by eto on 25/09/14.
  */
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.running;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import play.Logger;
+
 import com.baasbox.commands.CommandRegistry;
 import com.baasbox.commands.ScriptCommand;
 import com.baasbox.commands.ScriptCommands;
+import com.baasbox.commands.exceptions.CommandExecutionException;
 import com.baasbox.db.DbHelper;
 import com.baasbox.service.scripting.js.Json;
 import com.baasbox.service.storage.CollectionService;
 import com.baasbox.service.storage.DocumentService;
 import com.baasbox.service.user.UserService;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.BooleanNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.orientechnologies.orient.core.record.impl.ODocument;
-import org.apache.commons.lang.exception.ExceptionUtils;
-import static org.junit.Assert.*;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import static play.test.Helpers.*;
 
 public class ScriptDocumentCommandTest {
     private final static String TEST_USER = "script_command_test_user_"+ UUID.randomUUID();
@@ -72,14 +83,88 @@ public class ScriptDocumentCommandTest {
                 DbHelper.close(DbHelper.getConnection());
             } catch (Throwable e) {
                 fail(ExceptionUtils.getFullStackTrace(e));
-            }
+            }finally{
+           	 DbHelper.close(DbHelper.getConnection());
+           }
         });
     }
 
+    @Test
+    public void testGrantAndRevokeUpdate(){
+        running(fakeApplication(),()->{
+            try {
+            	//initial check. user TEST_ALT_USER cannot update the doc
+            	try{
+	                DbHelper.open("1234567890",TEST_ALT_USER,TEST_ALT_USER);
+		            	ObjectNode paramsUpdate = MAPPER.createObjectNode();
+		            	paramsUpdate.put("collection",TEST_COLLECTION);
+		            	paramsUpdate.put("id", sGenIds.get(0));
+			            paramsUpdate.put("data",MAPPER.readTree("{\"upd\":\"updValue\"}"));
+		            	ObjectNode cmdUpdate = ScriptCommands.createCommand("documents", "put",paramsUpdate);
+		            	JsonNode nodeUpdate =CommandRegistry.execute(cmdUpdate, null);
+	            
+	            	DbHelper.close(DbHelper.getConnection());
+	            	fail("The user should not update the doc, but it dit it!");
+            	}catch (CommandExecutionException e){
+            		
+            	}catch (Exception e){
+            		Logger.debug("OOOPS! something went wrong! ",e);
+            		fail(ExceptionUtils.getFullStackTrace(e));
+            		throw e;
+            	}finally{
+            		DbHelper.close(DbHelper.getConnection());
+            	}
+            	
+            	//use TEST_USER grant permission to update the doc to the user TEST_ALT_USER
+                DbHelper.open("1234567890",TEST_USER,TEST_USER);
+	                ObjectNode params = MAPPER.createObjectNode();
+	                ObjectNode users = MAPPER.createObjectNode();
+	                ArrayNode update = MAPPER.createArrayNode();
+	                update.add(TEST_ALT_USER);
+	                users.put("update", update);
+	                users.put("read", update);
+	                params.put("collection",TEST_COLLECTION);
+	                params.put("id", sGenIds.get(0));
+	                params.put("users",users);
+	                ObjectNode grant = ScriptCommands.createCommand("documents","grant",params);
+	                JsonNode node =CommandRegistry.execute(grant, null);
+                DbHelper.close(DbHelper.getConnection());
+
+                //now user TEST_ALT_USER can update the doc
+                DbHelper.open("1234567890",TEST_ALT_USER,TEST_ALT_USER);
+                	ObjectNode paramsUpdate = MAPPER.createObjectNode();
+                	paramsUpdate.put("collection",TEST_COLLECTION);
+                	paramsUpdate.put("id", sGenIds.get(0));
+ 	                paramsUpdate.put("data",MAPPER.readTree("{\"generated\":\"generated-123\",\"rand\":123,\"idx\":0,\"upd\":\"updValue\"}"));
+                	ObjectNode cmdUpdate = ScriptCommands.createCommand("documents", "put",paramsUpdate);
+                	JsonNode nodeUpdate =CommandRegistry.execute(cmdUpdate, null);
+                DbHelper.close(DbHelper.getConnection());
+                
+                //now the grant is revoked
+                DbHelper.open("1234567890",TEST_USER,TEST_USER);
+	                params = MAPPER.createObjectNode();
+	                users = MAPPER.createObjectNode();
+	                update = MAPPER.createArrayNode();
+	                update.add(TEST_ALT_USER);
+	                users.put("update", update);
+	                users.put("read", update);
+	                params.put("collection",TEST_COLLECTION);
+	                params.put("id", sGenIds.get(0));
+	                params.put("users",users);
+	                grant = ScriptCommands.createCommand("documents","revoke",params);
+	                node =CommandRegistry.execute(grant, null);
+                DbHelper.close(DbHelper.getConnection());
+            }catch (Throwable tr){
+            	Logger.debug(ExceptionUtils.getFullStackTrace(tr));
+                fail(ExceptionUtils.getFullStackTrace(tr));
+            }finally{
+            	 DbHelper.close(DbHelper.getConnection());
+            }
+        });}
 
 
     @Test
-    public void testGrantAndRevoke(){
+    public void testGrantAndRevokeRead(){
         running(fakeApplication(),()->{
             try {
                 DbHelper.open("1234567890",TEST_ALT_USER,TEST_ALT_USER);
@@ -138,7 +223,9 @@ public class ScriptDocumentCommandTest {
 
             }catch (Throwable tr){
                 fail(ExceptionUtils.getFullStackTrace(tr));
-            }
+            }finally{
+           	 DbHelper.close(DbHelper.getConnection());
+           }
         });
     }
 
