1b9284498476156efffda2fe78177dbea79ccee7
Claudio <claudio.github@gmail.com>
2015-05-13 18:17:53 +0200

    fixed OutOfMemory Error when restoring a BIG db and the server is in debug mode

--- app/com/baasbox/service/dbmanager/DbManagerService.java
+++ app/com/baasbox/service/dbmanager/DbManagerService.java
@@ -174,7 +174,7 @@ public class DbManagerService {
 				}else{
 					throw new FileFormatException("Looks like zip file does not contain a manifest file");
 				}
-				if (BaasBoxLogger.isDebugEnabled()) BaasBoxLogger.debug("Importing: "+fileContent);
+				if (BaasBoxLogger.isDebugEnabled()) BaasBoxLogger.debug("Importing: ",fileContent);
 				if(fileContent!=null && StringUtils.isNotEmpty(fileContent.trim())){
 					DbHelper.importData(appcode, fileContent);
 					zis.closeEntry();
