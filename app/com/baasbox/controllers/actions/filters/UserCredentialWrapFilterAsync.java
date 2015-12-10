caff8b8419956525f00f81f2f334ac783ba75d65
Claudio <claudio.github@gmail.com>
2015-12-10 09:48:43 +0100

    added check to prevent access as root against ODB

--- app/com/baasbox/controllers/actions/filters/UserCredentialWrapFilterAsync.java
+++ app/com/baasbox/controllers/actions/filters/UserCredentialWrapFilterAsync.java
@@ -75,6 +75,11 @@ public class UserCredentialWrapFilterAsync extends Action.Simple {
 			//one last thing: is the root user that is trying to access?
 			String username = (String)ctx.args.get("username");
 			String password = (String)ctx.args.get("password");
+			//the following check is necessary if we are using a remote connection because "root" is a valid user for ODB and we do not want to give direct access to the DB
+			if (username.equals("root") && !BBConfiguration.getInstance().isRootAsAdmin()){
+				tempResult=F.Promise.<SimpleResult>pure(unauthorized("User root is not authorized to access"));
+			}
+			//BTW if root can access as admin, we override its username
 			if (BBConfiguration.getInstance().isRootAsAdmin() && username.equals("root") && password.equals(BBConfiguration.getInstance().getRootPassword())){
 				//then override username and password
 				ctx.args.put("username", BBConfiguration.getInstance().getBaasBoxAdminUsername());
